db919ad71c62eee91eb01ca9c928df1b4bd9600a
Nedas <neduska991@gmail.com>
2022-02-28 14:11:13 +0000

    Test added

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5002;
 
+// For test
+app.get("/", (req, res) => res.send("Hello this is a test"));
+
 app.use(express.json());
 app.use(cors());
 
