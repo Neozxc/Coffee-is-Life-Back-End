9934085fe35875289df0b583e6e25ca291adf9b9
Akvile4 <aqua0818@gmail.com>
2022-02-28 23:18:47 +0200

    added update and delete

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { hashPassword, decryptPassword } = require("../middleware");
-const { addUser, listUsers, login } = require("./userController");
+const { addUser, listUsers, login, updateUser, updatePassword, deleteUser } = require("./userController");
 const userRouter = Router();
 
 userRouter.post("/user", hashPassword, addUser);
@@ -8,5 +8,11 @@ userRouter.post("/user", hashPassword, addUser);
 userRouter.get("/user", listUsers);
 
 userRouter.post("/login", decryptPassword, login);
+
+userRouter.put("/user", updateUser);
+
+userRouter.put("/password", decryptPassword, updatePassword);
+
+userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
