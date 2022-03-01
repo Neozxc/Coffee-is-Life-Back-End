f9d4f4fa10a785597e2464d95d9850b0f514c7ad
Akvile4 <aqua0818@gmail.com>
2022-03-01 18:30:55 +0200

    added search function

--- src/coffeeShops/shopController.js
+++ src/coffeeShops/shopController.js
@@ -19,3 +19,26 @@ exports.listShops = async (req, res) => {
         res.status(500).send({ err: error.message });
     }
 };
+
+exports.findShop = async (req, res) => {
+    try {
+        if (req.body.name) {
+            const shop = await Shop.find(
+                { name: {$regex: req.body.name} }
+            );
+            res.status(200).send({ shop });
+        }
+        else if (req.body.location) {
+            const shop = await Shop.find(
+                { location: {$regex: req.body.location} }
+            );
+            res.status(200).send({ shop });
+        }
+        else {
+            res.status(400).send({ error: "COULDN'T FIND WHAT YOU SEARCHING"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ err: error.message });
+    }
+};
