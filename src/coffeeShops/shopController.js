f4fe89530d0c74486fbc5b3dfe90ecc8edf81169
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-03-02 13:05:23 +0000

    Find shop details

--- src/coffeeShops/shopController.js
+++ src/coffeeShops/shopController.js
@@ -1,26 +1,26 @@
 const Shop = require("./shopModel");
 
 exports.addShop = async (req, res) => {
-    try {
-        const newShop = await Shop.create(req.body);
-        res.status(200).send({ shop: newShop });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error.message });
-    }
+	try {
+		const newShop = await Shop.create(req.body);
+		res.status(200).send({ shop: newShop });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ err: error.message });
+	}
 };
 
 exports.listShops = async (req, res) => {
-    try {
-        const shops = await Shop.find({ });
-        res.status(200).send({ shops });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error.message });
-    }
+	try {
+		const shops = await Shop.find({});
+		res.status(200).send({ shops });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ err: error.message });
+	}
 };
 
-exports.findShop = async (req, res) => {
+/* exports.findShop = async (req, res) => {
     try {
         if (req.body.name) {
             const shop = await Shop.find(
@@ -41,4 +41,15 @@ exports.findShop = async (req, res) => {
         console.log(error);
         res.status(500).send({ err: error.message });
     }
+}; */
+
+exports.findShop = async (req, res) => {
+	try {
+		const shop = await Shop.findOne({ name: req.body.name });
+		console.log(shop)
+		res.status(200).send({ shop });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ err: error.message });
+	}
 };
