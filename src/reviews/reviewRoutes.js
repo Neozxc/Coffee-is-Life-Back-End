9987267353094b78b0f34789a052c3709cb5b6fe
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-03-11 04:10:24 +0000

    Add updateReview and updateRating controllers/routes

--- src/reviews/reviewRoutes.js
+++ src/reviews/reviewRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { addReview, reviewList, deleteReview, updateReview } = require("./reviewController");
+const { addReview, reviewList, deleteReview, updateReview, updateRating } = require("./reviewController");
 const reviewRouter = Router();
 
 reviewRouter.post("/review", addReview);
 reviewRouter.get("/reviewlist", reviewList);
 reviewRouter.delete("/review", deleteReview);
-// reviewRouter.patch("/review", updateReview);
+reviewRouter.put("/review", updateReview);
+reviewRouter.put("/rating", updateRating);
 
 module.exports = reviewRouter;
